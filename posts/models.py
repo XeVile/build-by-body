57dea69c93c09c8a47eaab9b736737fdd7dc858a
xevile <xevile.arunabh@gmail.com>
2021-11-20 11:36:36 +0800

    Build By Body - First Commit

---
+++ posts/models.py
@@ -0,0 +1,35 @@
+import datetime
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+# FOR BLOG FRAMEWORK
+class Post(models.Model):
+    title = models.CharField(max_length=333)
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(default=timezone.now)
+    pub_date = models.DateTimeField(blank=True, null=True)
+
+    post_body = models.TextField(max_length=100000)
+
+    def __str__(self):
+        return self.title
+
+    def published(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Music(models.Model):
+    track_title = models.CharField(max_length=333)
+    album_artist = models.CharField(max_length=333)
+    album_title = models.CharField(max_length=333)
+
+    def __str__(self):
+        return self.track_title + '-' + self.album_artist
+
+class Product_recommend(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=200, default='Product Placeholder')
+    amazon_link = models.CharField(max_length=65535, default='https://www.amazon.com/')
---
+++ posts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def index(request):
+    latest_post_list = Post.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_post_list': latest_post_list,
+    }
+    return render(request, '_layouts/home.html',context)
+
+def expand(request, post_id):
+    try:
+        post = Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404("Post does not exist")
+    return render(request, '_layouts/expand.html')
+
